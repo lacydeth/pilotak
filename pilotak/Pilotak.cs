d39661838eca3c5ad30e760a7bb76f996fc60ef6
white <white@DESKTOP-D1M2GIS>
2023-05-19 09:59:47 +0200

    szünet előtti mentés

---
+++ pilotak/Pilotak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pilotak
+{
+    internal class Pilotak
+    {
+        string versenyzoNev;
+        string versenyzoSzuletes;
+        string versenyzoNemzetiseg;
+        int versenyzoRajtszam;
+
+        public Pilotak(string versenyzoNev, string versenyzoSzuletes, string versenyzoNemzetiseg, int versenyzoRajtszam)
+        {
+            this.versenyzoNev = versenyzoNev;
+            this.versenyzoSzuletes = versenyzoSzuletes;
+            this.versenyzoNemzetiseg = versenyzoNemzetiseg;
+            this.versenyzoRajtszam = versenyzoRajtszam;
+        }
+
+        public string VersenyzoNev { get => versenyzoNev; }
+        public string VersenyzoSzuletes { get => versenyzoSzuletes; }
+        public string VersenyzoNemzetiseg { get => versenyzoNemzetiseg; }
+        public int VersenyzoRajtszam { get => versenyzoRajtszam; }
+    }
+}
--- pilotak/Program.cs
+++ pilotak/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using pilotak;
+using System.Text;
+
+
+List<Pilotak> pilotak = new List<Pilotak>();
+
+
+StreamReader sr = new StreamReader("pilotak.csv");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    string[] adat = sr.ReadLine().Split(';');
+    pilotak.Add(new Pilotak(adat[0], adat[1], adat[2], (adat[3].Length > 0) ? int.Parse(adat[3]) : 0));
+}
+sr.Close();
+
+Console.WriteLine($"3. feladat: {pilotak.Count} fő");
